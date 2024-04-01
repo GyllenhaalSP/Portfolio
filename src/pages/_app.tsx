22624e67d4c93d8564db4eb38fab6f6b52decc3f
Gyllenhaal <dalonsolaz@gmail.com>
2024-04-01 12:45:22 +0200

    Añadidas etiquetas meta con datos de autoría y con los favicons para varias plataformas.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,18 +6,155 @@ const App = ({ Component, pageProps }) => {
   const inputRef = React.useRef<HTMLInputElement>(null);
 
   const onClickAnywhere = () => {
-    inputRef.current.focus();
+    inputRef.current?.focus();
   };
 
   return (
     <>
       <Head>
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
         <meta
-          name="viewport"
-          content="initial-scale=1.0, width=device-width"
-          key="viewport"
-          maximum-scale="1"
+          name="description"
+          content="Portfolio de Daniel Alonso Lázaro - www.dalonsolaz.dev"
         />
+        <meta
+          name="copyright"
+          content="&copy; 2024, Daniel Alonso Lázaro - www.dalonsolaz.dev"
+        />
+        <meta name="author" content="Daniel Alonso Lázaro" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        {/* Apple Icons */}
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-icon-180x180.png"
+        />
+
+        {/* Android Icons */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="36x36"
+          href="/android-icon-36x36.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="48x48"
+          href="/android-icon-48x48.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="72x72"
+          href="/android-icon-72x72.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/android-icon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="144x144"
+          href="/android-icon-144x144.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+
+        {/* Microsoft Icons */}
+        <meta
+          name="msapplication-square70x70logo"
+          content="/ms-icon-70x70.png"
+        />
+        <meta
+          name="msapplication-square150x150logo"
+          content="/ms-icon-150x150.png"
+        />
+        <meta
+          name="msapplication-wide310x150logo"
+          content="/ms-icon-310x310.png"
+        />
+        <meta
+          name="msapplication-square310x310logo"
+          content="/ms-icon-310x310.png"
+        />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+
+        {/* General Icons */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+
+        {/* Manifest & Browser Configuration */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="msapplication-config" content="public/browserconfig.xml" />
+        <meta name="robots" content="index, follow" />
       </Head>
 
       <div
